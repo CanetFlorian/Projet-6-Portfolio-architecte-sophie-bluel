cb8900e9f8a9e03af662481a8c0bc1b922f38290
Canet Florian <florian.canet12@gmail.com>
2025-01-31 11:46:37 +0100

    fetch des projets api

--- FrontEnd/assets/script/script.js
+++ FrontEnd/assets/script/script.js
@@ -1,3 +1,23 @@
 const reponse = await fetch (`http://localhost:5678/api/works`);
 const works = await reponse.json();
 
+function genererWorks(works) {
+    for(let i = 0; i < works.length; i++) {
+        const projet = works[i];
+        const sectionProjet = document.querySelector(".gallery");
+        const articleProjet = document.createElement("figure");
+        
+    
+        
+        const imageElement = document.createElement("img");
+        imageElement.src = projet.imageUrl;
+        const nomElement = document.createElement("figcaption");
+        nomElement.textContent = projet.title;
+
+        sectionProjet.appendChild(articleProjet);
+        articleProjet.appendChild(imageElement);
+        articleProjet.appendChild(nomElement);
+    }
+}
+
+genererWorks(works);
