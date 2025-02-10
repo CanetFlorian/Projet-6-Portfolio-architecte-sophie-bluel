a18597b6ec514de0be2fcd7e41f5182845818b07
Canet Florian <florian.canet12@gmail.com>
2025-02-10 15:14:43 +0100

    Ajout 2nd fenetre modale

--- FrontEnd/assets/script/modale.js
+++ FrontEnd/assets/script/modale.js
@@ -1,16 +1,18 @@
 //fonction pour ouvrir modale
-const openModal = function(event) {
-    event.preventDefault();
-
-    const target = document.querySelector(event.target.getAttribute("href"));
-    target.style.display = "flex";
-    target.removeAttribute("aria-hidden");
-    target.setAttribute("aria-modal", "true");
+const openModal = function(modal) {
+    
+    modal.style.display = "flex";
+    modal.removeAttribute("aria-hidden");
+    modal.setAttribute("aria-modal", "true");
 }
 
 // evenement pour appeler la fonction qui va ouvrir la modale 
 const modaleModifier = document.querySelector(".js-modal");
-modaleModifier.addEventListener("click", openModal);
+modaleModifier.addEventListener("click", function(event) {
+    event.preventDefault();
+    const target = document.querySelector(event.target.getAttribute("href"));
+    openModal(target);
+});
 
 //fonction pour fermer la modale 
 
@@ -26,10 +28,40 @@ const closeModal = function(event) {
 }
 //evenement pour appeler la fonction qui va fermer la modale au clic sur la X 
 
-const modaleCloseCroix = document.querySelector(".close-modal");
-modaleCloseCroix.addEventListener("click", closeModal);
+const modaleCloseCroix = document.querySelectorAll(".close-modal");
+modaleCloseCroix.forEach(button => {
+    button.addEventListener("click", closeModal);
+});
+
 
 // evenement pour appeler la fonction qui va fermer la modale au clic en dehors
 
-const modaleCloseOut = document.querySelector(".modal");
-modaleCloseOut.addEventListener("click", closeModal);
+const modaleCloseOut = document.querySelectorAll(".modal");
+modaleCloseOut.forEach(modal => {
+    modal.addEventListener("click", closeModal);
+});
+
+
+// fonction pour ouvrir la 2nd modale
+
+const addPhotoBtn = document.querySelector("#addPhotoBtn");
+addPhotoBtn.addEventListener("click", function() {
+    // Fermer la première modale
+    closeModal({ target: document.querySelector("#modal1") });
+
+    // Ouvrir la seconde modale
+    const modal2 = document.querySelector("#modal2");
+    openModal(modal2);
+});
+
+
+// fonction retour modal 2
+
+const returnModal = document.querySelector("#returnModal");
+returnModal.addEventListener("click", function() {
+    
+    closeModal({target: document.querySelector("#modal2")});
+
+    const modal1 = document.querySelector("#modal1");
+    openModal(modal1);
+});
